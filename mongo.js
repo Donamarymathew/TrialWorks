3b1d14d7b29302a526abe570223cd535108bfd65
Donamarymathew <donamarymathew97@gmail.com>
2020-05-20 10:58:28 +0530

    false commit

---
+++ mongo.js
@@ -0,0 +1,23 @@
+db.books.insert(
+    {Name:"Human Universe",
+    Author:["Brian Cox", "Andrew Cohen"],
+    Binding:["Hardback","Paperback","e-book"],
+    publisher:"William Collins",
+    PublishedIn:2015,
+    NoOfCopies:{Paperback:16,Hardback:10},
+    Cost:{"Paperback":400,"Hardback":1400,"e-book":300},
+    Description:"This book asks questions about our origins, our destiny and our place in the universe",
+    Classified:["earth sciences", "astrophysics", "science", "evolution"]
+}
+)
+
+db.books.insert(
+    {Name:"The Origin of Species",
+    Author:"Charles Darwin",
+    Binding:["Hardback"],
+    NoOfCopies:{Hardback:0451529065},
+    ISBN:{Hardback:10},
+    Cost:{Hardback:800},
+    Classified:["biology", "life sciences", "evolution", "science"]
+}
+)
